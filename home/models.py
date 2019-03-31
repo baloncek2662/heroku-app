121697bd0593ce9ae0a4093b40b0f68c102793bc
baloncek2662 <martic.gasper@gmail.com>
2019-03-31 23:18:19 +0200

    add new pages, templates

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username_text = models.CharField(db_column='Username', max_length=50)
+    first_name_text = models.CharField(db_column='FirstName', max_length=50)
+    last_name_text = models.CharField(db_column='LastName', max_length=50)
+    join_date = models.DateTimeField(db_column='date joined')
+    def __str__(self):
+        return self.username_text + ' (' + self.first_name_text + ' ' + self.last_name_text + ')'
+
+class AccountType(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_type_text = models.CharField(db_column='AccountType', max_length=50)
+    def __str__(self):
+        return self.account_type_text
--- home/views.py
+++ home/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import User
 
 def index(request):
-    return HttpResponse("Hello, world. You're at this your homepage.")
+    users = User.objects.all()
+    template = loader.get_template('home/index.html')
+    context = {
+        'users': users,
+    }
+    return HttpResponse(template.render(context, request))
+
+def register(request):
+    return HttpResponse("You are at the registration page")
+
+def login(request):
+    return HttpResponse("You are at the login page")
